863bd473886764ae1cef337085fa3ad933a2505b
Chrystal <cchav083@ucr.edu>
2024-05-02 15:46:42 -0700

    add comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import praw
 
 reddit = praw.Reddit(
@@ -6,12 +7,25 @@ reddit = praw.Reddit(
     user_agent = "cs172 crawler"
 )
 
+saved_top_posts = [[]]
+
 subreddit = reddit.subreddit("python")
 
 top_posts = subreddit.top(limit=10)
 new_posts = subreddit.new(limit=10)
 
 for post in top_posts:
+    saved_top_posts.append(post.title)
+    print("ID - ", post.id)
+    print("Author - ", post.author)
+    print("URL - ", post.url)
+    print("Score - ", post.score)
+    print("Comment count - ",post.num_comments)
+    print("Created - ", post.created_utc)
+    print("\n")
+  
+
+for post in new_posts:
     print("Title - ", post.title)
     print("ID - ", post.id)
     print("Author - ", post.author)
@@ -20,4 +34,9 @@ for post in top_posts:
     print("Comment count - ",post.num_comments)
     print("Created - ", post.created_utc)
     print("\n")
-    
+    post_comments = reddit.submission(id = post.id)
+    comments = post_comments.comments
+    for comment in comments:
+        print("Printing Comment ...")
+        print("Comment: ", comment.body)
+        print("\n")
