aa5073633dd0dbde588cb00538655471014d4c96
wilki52 <93832033+wilki52@users.noreply.github.com>
2024-05-04 06:30:23 -0700

    Add files via upload

---
+++ userinfo.py
@@ -0,0 +1,10 @@
+
+platform = "mac" #change
+username = '/u/username'
+
+useragent = (platform +":com.group31.cs172crawl:v0.0.1 (by " + username +")")
+
+#WRITE UR OWN : template for useragent : <platform>:<app ID>:<version string> (by /u/<reddit username>)
+
+#you only have to change platform and username lol
+
